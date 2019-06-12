d38e3db9122f1d36a49bf63b2c47016707e9d2d4
OKAUEND <38197580+OKAUEND@users.noreply.github.com>
2019-06-13 00:20:39 +0900

    Qiita投稿用のファイルを作成/プッシュ

---
+++ index.js
@@ -0,0 +1,66 @@
+window.addEventListener('load',function()
+{
+    const submit_buttom = this.document.querySelector('.submit');
+    const delete_buttom = this.document.querySelector('.delete');
+
+    submit_buttom.addEventListener('click',() =>
+    {
+        modalWindowOpen('OKモード','OK','js-modalButton--submit');
+    })
+
+    delete_buttom.addEventListener('click',() =>{
+        modalWindowOpen('削除モード','削除','js-modalButton--deleting');
+    })
+
+    function modalWindowOpen(titleText,buttonText,submitClassName)
+    {
+        //モーダルウィンドウを展開する処理へ変更
+        //モーダルウィンドウの各種を表示
+        document.querySelector('#modal').classList.remove('modal--is_show');
+        document.querySelector('.modal__body').classList.remove('modal--is_show');
+        document.querySelector('.modal__back').classList.remove('modal__back--is_active');
+    
+        //タイトルを設定する
+        document.querySelector('.modal__title--text').appendChild(document.createTextNode(titleText));
+    
+        const modalSubmitbutton = document.querySelector('.modalButtom__Submit');
+        //Submitボタンの名前を設定する
+        modalSubmitbutton.appendChild(document.createTextNode(buttonText));
+    
+        //Submitのクラス名を設定する
+        modalSubmitbutton.classList.add(submitClassName);
+    
+        //モーダルウィンドウのバック黒画面をクリックしたときのイベントを追加する
+        document.querySelector('.modal__back').addEventListener('click',modalWindowClose,false);
+        document.querySelector('.modalButtom__Cancel').addEventListener('click',modalWindowClose,false);
+        document.querySelector('.modal__close').addEventListener('click',modalWindowClose,false);
+
+    }
+    function modalWindowClose()
+    {    
+        //モーダルウィンドウを非表示に
+        document.querySelector('#modal').classList.add('modal--is_show');
+        document.querySelector('.modal__body').classList.add('modal--is_show');
+        document.querySelector('.modal__back').classList.add('modal__back--is_active');
+    
+        //モーダルウィンドウのタイトル部分にあたるテキストを削除する
+        //最初の要素がテキストの要素であることが確定なので、firstchildで指定しそれをremoveする
+        const modalWindowTitleText = document.querySelector('.modal__title--text');
+        modalWindowTitleText.removeChild(modalWindowTitleText.firstChild);
+    
+        //Submit用ボタンのテキストを削除する
+        const modalSubmitbutton = document.querySelector('.modalButtom__Submit');
+        modalSubmitbutton.removeChild(modalSubmitbutton.firstChild);
+    
+        //Submit用ボタンのスタイル指定を解除する
+        const classList = modalSubmitbutton.classList;
+        modalSubmitbutton.classList.remove(classList[classList.length -1]);
+    
+        //表示しているアイテムをすべて削除する
+        const modalWindowNodeList = document.querySelector('.modal__item');
+        modalWindowNodeList.textContent = null;
+    
+        //モーダルウィンドウのバック黒画面をクリックしたときのイベントを削除
+        document.querySelector('.modal__back').removeEventListener('click',modalWindowClose,false);
+    }
+},false)
